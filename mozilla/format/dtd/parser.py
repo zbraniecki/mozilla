284154587c66c4c204bbdf9f79fa8222208e54cd
Zbigniew Braniecki <gandalf@mozilla.com>
2011-07-04 21:18:42 +0200

    update dtd parser

--- mozilla/format/dtd/parser.py
+++ mozilla/format/dtd/parser.py
@@ -18,39 +18,74 @@ class Parser():
     }
 
     @classmethod
-    def parse(cls, text):
+    def parse(cls, text, struct=True):
         dtd = ast.DTD()
-        dtd._struct = []
-        cls.split_comments(text, dtd)
+        if struct:
+            dtd._struct = []
+            cls.split_comments(text, dtd, struct=struct)
+        else:
+            text = cls.remove_comments(text)
+            for match in cls.patterns['entity'].findall(text):
+                dtd.body.append(ast.Entity(match[2], match[5]))
         return dtd
+    """
+    @classmethod
+    def parse_to_idlist(cls, text):
+        text = cls.patterns['comment'].sub('', text)
+        ids = [m[0] for m in cls.patterns['id'].findall(text)]
+        return ids
 
     @classmethod
-    def split_comments (cls, text, dtd, pointer=0):
+    def parse_to_entitylist(cls, text):
+        elist = EntityList(id=None)
+        text = cls.patterns['comment'].sub('', text)
+        for match in cls.patterns['entity'].findall(text):
+            elist.add(Entity(match[0], match[1][1:-1]))
+        return elist
+
+    @classmethod
+    def parse_entity(cls, text):
+        match = self.patterns['entity'].match(text)
+        if not match:
+            raise Exception()
+        entity = Entity(match.group(0))
+        entity.set_value(match.group(1)[1:-1])
+        return entity
+    """
+
+    @classmethod
+    def remove_comments(cls, text):
+        return cls.patterns['comment'].sub('', text)
+
+    @classmethod
+    def split_comments (cls, text, dtd, pointer=0, struct=True):
         pattern = cls.patterns['comment']
         match = pattern.search(text)
         while match:
             st0 = match.start(0)
-            cls.split_entities(text, dtd, pointer=pointer, end=st0)
+            cls.split_entities(text, dtd, pointer=pointer, end=st0, struct=struct)
             comment = ast.Comment(match.group(1))
             dtd.body.append(comment)
             pointer = match.end(0)
             match = pattern.search(text, pointer)
         if len(text) > pointer:
-            cls.split_entities(text, dtd, pointer=pointer)
+            cls.split_entities(text, dtd, pointer=pointer, struct=struct)
 
     @classmethod
-    def split_entities (cls, text, dtd, pointer=0, end=sys.maxsize):
+    def split_entities (cls, text, dtd, pointer=0, end=sys.maxsize, struct=True):
         pattern = cls.patterns['entity']
         match = pattern.search(text, pointer, end)
         while match:
             st0 = match.start(0)
-            dtd._struct.append(text[pointer:st0])
+            if struct:
+                dtd._struct.append(text[pointer:st0])
             entity = ast.Entity(match.group(2), match.group(5))
-            entity._struct = (match.group(1), match.group(3), match.group(6))
-            entity._value_p = match.group(4)
+            if struct:
+                entity._struct = (match.group(1), match.group(3), match.group(6))
+                entity._value_p = match.group(4)
             dtd.body.append(entity)
             pointer = match.end(0)
             match = pattern.search(text, pointer, end)
-        if len(text) > pointer:
+        if struct and len(text) > pointer:
             dtd._struct.append(text[pointer:end])
 
